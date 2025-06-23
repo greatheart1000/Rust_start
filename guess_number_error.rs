d5cfb5802e6024dc66015e07cd97f51645b4cb81
greatheart1000 <64632461+greatheart1000@users.noreply.github.com>
2025-06-23 17:44:53 +0800

    Update guess_number_error.rs

--- guess_number_error.rs
+++ guess_number_error.rs
@@ -34,3 +34,16 @@ fn main() {
         }
     }
 }
+
+
+use std::io;
+
+fn main() {
+   let a=[1,2,3,4,5];
+   println!("Please enter an array index.");
+   let mut index = String::new();
+   io::stdin().read_line(&mut index).expect("not a number");
+   let index:usize =index.trim().parse().expect("expect a number");
+   let element = a[index];
+   println!("The value of the element at index {index} is {element}") ;   
+}
