159bd0b7e3a6a215c36363fb303ea59c591857d5
greatheart1000 <64632461+greatheart1000@users.noreply.github.com>
2025-06-23 18:10:44 +0800

    Update guess_number_error.rs

--- guess_number_error.rs
+++ guess_number_error.rs
@@ -47,3 +47,28 @@ fn main() {
    let element = a[index];
    println!("The value of the element at index {index} is {element}") ;   
 }
+
+//在函数签名中，必须 声明每个参数的类型 
+fn main() {
+  println!("hello world");
+  another_function(10);
+   
+}
+fn another_function(x:u32){
+    println!("haha learn function {x}");
+}
+
+fn main() {
+   let x=five();
+   println!("the number is {x}");
+   let y=plus_one(10);
+   println!("y is {y}");
+}
+
+fn five()->u32{
+    5
+} //不能有分号 移除分号，现在 5 是一个表达式，其值被隐式返回 或者 return 5;
+
+fn plus_one(x:u32)->u32{ //这里必须要有一个返回的类型
+    return x+1;
+}
