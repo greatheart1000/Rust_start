912d7435e19d77698dcfa81247c7b182e1b3ea39
greatheart1000 <greatheart1000@163.com>
2025-07-22 18:54:38 +0800

    feat: add marco_demo example

---
+++ marco_demo/target/debug/deps/marco_demo-7927184b330fa353.d
@@ -0,0 +1,5 @@
+/mnt/d/project/Rust_project/Rust_start/marco_demo/target/debug/deps/marco_demo-7927184b330fa353.d: src/main.rs
+
+/mnt/d/project/Rust_project/Rust_start/marco_demo/target/debug/deps/libmarco_demo-7927184b330fa353.rmeta: src/main.rs
+
+src/main.rs:
---
+++ marco_demo/target/debug/deps/marco_demo-efe0363941bfa96a.d
@@ -0,0 +1,5 @@
+/mnt/d/project/Rust_project/Rust_start/marco_demo/target/debug/deps/marco_demo-efe0363941bfa96a.d: src/main.rs
+
+/mnt/d/project/Rust_project/Rust_start/marco_demo/target/debug/deps/libmarco_demo-efe0363941bfa96a.rmeta: src/main.rs
+
+src/main.rs:
